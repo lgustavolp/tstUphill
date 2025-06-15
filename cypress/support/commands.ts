28dd180b847dad233fb5388b4ad8eb0b91e751ba
Luiz Gustavo Lucena <lgustavolp@gmail.com>
2025-06-15 14:02:02 -0300

    fix errors

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,13 @@
+// Extend Cypress' Chainable interface to include custom commands
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      loginByApi(): Chainable<void>;
+      getByCy(sel: string): Chainable<JQuery<HTMLElement>>;
+    }
+  }
+}
+
 Cypress.Commands.add('loginByApi', () => {
   cy.request('POST', '/api/login', { user: 'qa', pass: 'qa' })
     .its('body.token').then(t => window.localStorage.setItem('token', t));
@@ -6,3 +16,5 @@ Cypress.Commands.add('loginByApi', () => {
 Cypress.Commands.add('getByCy', (sel:string) =>
   cy.get(`[data-cy=${sel}]`)
 );
+
+export {};
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,4 +1,19 @@
 import './commands';
 import 'cypress-axe';
+
+declare global {
+  namespace Cypress {
+	interface Chainable {
+	  injectAxe(): Chainable<void>;
+	  checkA11y(
+		context?: any,
+		options?: any,
+		violationCallback?: (violations: any) => void,
+		skipFailures?: boolean
+	  ): Chainable<void>;
+	}
+  }
+}
+
 beforeEach(() => cy.injectAxe());
 afterEach(() => cy.checkA11y(undefined, undefined, undefined, true));
