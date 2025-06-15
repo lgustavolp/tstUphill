d3f08b43ef0a405f78df0c59952801981c034670
Luiz Gustavo Lucena <lgustavolp@gmail.com>
2025-06-15 15:49:19 -0300

    fix(config): move support file from index.ts to e2e.ts and adjust Cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,14 @@ import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 
 export default defineConfig({
-  baseUrl: 'https://uphillhealth.com/uphillchallenge/desk?routePackageId=ROUTE_PACKAGE_AS_CHALLENGE&page=0&tab=2&phasesIds=*',
   e2e: {
-    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'https://uphillhealth.com/uphillchallenge/desk?routePackageId=ROUTE_PACKAGE_AS_CHALLENGE&page=0&tab=2&phasesIds=*',
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on('file:preprocessor', createBundler());
       return config;
-    }
+    },
+    specPattern: 'cypress/e2e/**/*.feature'
   },
   video: true
 });
